58d3916f6039f32861710a9f805ed532297bddc8
Paul-Williams <p.b.williams@outlook.com>
2021-05-19 15:25:44 +0100

    Add project files.

---
+++ PW.Drawing/BitmapExtensions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace PW.Drawing
+{
+  public static class BitmapExtensions
+  {
+
+    public static (Bitmap, Size) GetHDSize(this Bitmap image)
+    {
+      if (image is null)
+      {
+        throw new ArgumentNullException(nameof(image));
+      }
+
+      var h = image.Size.Height;
+      var w = image.Size.Width;
+
+      // Get the image's original width and height
+      int originalWidth = image.Width;
+      int originalHeight = image.Height;
+
+      // To preserve the aspect ratio
+      float ratioX = (float)HD.Width / originalWidth;
+      float ratioY = (float)HD.Height / originalHeight;
+      float ratio = Math.Min(ratioX, ratioY);
+
+      float sourceRatio = (float)originalWidth / originalHeight;
+
+      // New width and height based on aspect ratio
+      int newWidth = (int)(originalWidth * ratio);
+      int newHeight = (int)(originalHeight * ratio);
+
+
+      return (image, new Size(newWidth, newHeight));
+
+      //// Portrait
+      //if (image.Size.Height > image.Size.Width)
+      //{
+      //  var ratio = h / (decimal)w;
+
+      //  return (image, new Size(1920, Convert.ToInt32(1080 * ratio)));
+
+      //}
+
+      //// Landscape
+      //else if (image.Size.Width > image.Size.Height)
+      //{
+      //  var ratio = w / (decimal)h;
+      //  return (image, new Size(Convert.ToInt32(1920 * ratio), 1080));
+      //}
+
+      ////Square
+      //else
+      //{
+      //  return (image, new Size(1080, 1080));
+      //}
+
+
+    }
+
+    public static Bitmap ResizeTo(this (Bitmap image, Size size) t) => t.image.Resize(t.size);
+
+    public static Bitmap Resize(this Bitmap image, Size size) => image.Resize(size.Width, size.Height);
+
+
+    /// <summary>
+    /// Resize the image to the specified width and height.
+    /// </summary>
+    public static Bitmap Resize(this Bitmap image, int width, int height)
+    {
+      // See: https://stackoverflow.com/questions/1922040/how-to-resize-an-image-c-sharp
+
+      var destRect = new Rectangle(0, 0, width, height);
+      var destImage = new Bitmap(width, height);
+
+      destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+      using (var graphics = Graphics.FromImage(destImage))
+      {
+        graphics.CompositingMode = CompositingMode.SourceCopy;
+        graphics.CompositingQuality = CompositingQuality.HighQuality;
+        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        graphics.SmoothingMode = SmoothingMode.HighQuality;
+        graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+        using var wrapMode = new ImageAttributes();
+        wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+        graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+      }
+
+      return destImage;
+    }
+
+    public static void SaveHQ(this Bitmap bitmap, string path) => bitmap.SaveHQ(path, 95L);
+
+    public static void SaveHQ(this Bitmap bitmap, string path, long compression)
+    {
+      if (bitmap is null) throw new ArgumentNullException(nameof(bitmap));
+      if (string.IsNullOrEmpty(path)) throw new ArgumentException($"'{nameof(path)}' cannot be null or empty.", nameof(path));
+
+
+      var imageCodecInfo = ImageHelper.GetEncoderInfo("image/jpeg");
+
+      if (imageCodecInfo is null) throw new Exception("Unable to get imageCodecInfo for: image/jpeg");
+
+      var encoderParameters = new EncoderParameters(1);
+      encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, compression);
+
+      bitmap.Save(path, imageCodecInfo, encoderParameters);
+    }
+
+
+    public static bool IsBiggerThanHD(this Image image) =>
+      image is not null ? image.Size.Height > HD.Height || image.Size.Width > HD.Width
+      : throw new ArgumentNullException(nameof(image));
+
+
+    public static Size HD { get; } = new Size(1920, 1080);
+
+  }
+}
---
+++ PW.Drawing/ImageHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PW.Drawing
+{
+  public static class ImageHelper
+  {
+    // See: https://efundies.com/csharp-save-jpg/
+    public static ImageCodecInfo? GetEncoderInfo(String mimeType)
+    {
+      int j;
+      ImageCodecInfo[] encoders;
+      encoders = ImageCodecInfo.GetImageEncoders();
+      for (j = 0; j < encoders.Length; ++j)
+      {
+        if (encoders[j].MimeType == mimeType)
+          return encoders[j];
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// Loads an image from file. Unlike <see cref="Image.FromFile(string)"/> it does not hold a lock on the file until the <see cref="Image"/> is disposed.
+    /// This does not support Vector images, only Bitmaps.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static Image ImageFromFile(string path)
+    {
+      // Purpose:
+      // This method is a work-around for a common problem where loading an Image into a PictureBox
+      // results in the file on disk staying locked even after the Image is removed from the PictureBox
+      // and disposed. The picture file cannot then be deleted or moved until the application is closed.
+      // Not sure if the fault is with Dispose() or whether the PictureBox has anything to do with it.
+
+      // Usage:
+      // Have tested the following way of using this method and successfully been able to delete all images
+      // without closing the caller application.
+
+      // Example:
+      // using (PictureBox.Image) { PictureBox.Image = ImageHelper.ImageFromFile(filePath);
+
+
+      // Logic:
+      // The first bitmap created from file will hold a lock until disposed.
+      // A second bitmap is created from the first one, and does not know about the file.
+      // In this way the file is not locked for ages!
+      using var tmp = new Bitmap(path);
+      return new Bitmap(tmp);
+
+    }
+  }
+}
